b206b3069e1c425e5994c61574a72a3868cb1a9b
Malak Odtalla <malakodt35@gmail.com>
2022-06-11 22:26:21 +0300

    add changes

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -1,71 +1,104 @@
+import re
+
 class Musician:
 
-    members = []
-
-    def __init__(self, role, instrument):
-        self.role = role
-        self.instrument = instrument
-        self.__class__.members.append(self)
 
 
+  def __init__(self, name):
+    self.name = name
 
-    def __str__(self):
-        return f'I am a {self.role}'
+  def __repr__(self):
+    return 'Name: ' + self.name + '\nInstrument: ' + self.get_instrument
 
+  def __str__(self):
+    return self.name + ' on ' + self.get_instrument() + '\n'
 
-    def __repr__(self):
-        return self.role
+  def play_solo(self):
+    return self.__class__.solo
 
-    def play_solos(self):
-        return f'{self.role} is playing solo on the {self.instrument}'
-
-
-    def get_instrument(self):
-        return self.instrument
-
+  def get_instrument(self):
+    return self.__class__.instrument
 
 class Guitarist(Musician):
-
-
-    pass
-
-class Bassist(Musician):
-
-
-    pass
+      solo = 'Guitar Sounds'
+      instrument = 'Guitar'
 
 class Drummer(Musician):
+  solo = 'LOUD NOISES'
+  instrument = 'Drums'
 
-    pass
+class Bassist(Musician):
+  solo = 'Bass Sounds'
+  instrument = 'Bass'
 
 class Band:
 
-    all = []
+  all_bands = []
+
+  def __init__(self, name, members = []):
+    self.name = name
+    self.members = members
+    self.__class__.all_bands.append(self)
+
+  def __repr__(self):
+    output = 'Name: ' + self.name + '\n' + '# of members: ' + len(self.members)
+    return output
+
+  def __str__(self):
+    output = 'The Band\'s Name is: ' + self.name + '\n'
+    output += 'The Members are:\n'
+    for person in self.members:
+      output += person.__str__()
+    return output
+
+  def play_solos(self):
+    output = ''
+    for person in self.members:
+      output += person.play_solo() + ' '
+
+    return output
+
+  @staticmethod
+  def create_from_data(data):
+    split_data = re.findall(r"\S.*", data)
+    print(split_data)
+
+    title = split_data[0]
+    members = []
+    for i in range(1, len(split_data)):
+      current_member = split_data[i].split(',')
+
+      if current_member[1] == 'Guitar':
+        members.append(Guitarist(current_member[0]))
+      elif current_member[1] == 'Bass':
+        members.append(Bassist(current_member[0]))
+      elif current_member[1] == 'Drums':
+        members.append(Drummer(current_member[0]))
+      else:
+        members.append(Musician(current_member[0]))
 
 
-    def __init__(self, name, members=[]):
-
-        self.name = name
-        self.members = members
-        self.__class__.all.append(self)
+    return Band(title, members)
 
 
-    def play_solos(self):
+  @classmethod
+  def to_list(cls):
+    output = ''
+    for current_band in cls.all_bands:
+      output += current_band.__str__()
 
-        solos = ''
-        for i in self.members:
-            solos += f'{i.play_solo()}\n'
-        return solos[:-1]
+    return output
 
 
-    def __str__(self):
-        return ("We are the " + self.name)
-
-    def __repr__(self):
-
-        return ("This is the " + self.name)
-
-    @classmethod
-    def to_list(ins):
-
-        return ins.all
+# if __name__ == "__main__":
+#
+#   band_one = """
+#   Catfish and the Bottlemen
+#   Van McCann,Guitar
+#   Jon Barr,Drums
+#   Benji Blakeway,Bass
+#   Bob Hall,Guitar
+#   """
+#
+#   print(Band.create_from_data(band_one))
+#
--- tests/test_pythonic_garage_band.py
+++ tests/test_pythonic_garage_band.py
@@ -1,8 +1,113 @@
-from pythonic_garage_band.pythonic_garage_band import Guitarist, Drummer, Bassist, Band, Musician
+from pythonic_garage_band.pythonic_garage_band import  Musician, Band,Guitarist, Bassist, Drummer
 import pytest
 
-def test_guitarist_play_solos():
-    expected = 'Guitarist is playing solo on the guitar'
-    actual = Guitarist('Guitarist', 'guitar').play_solos()
-    assert expected == actual
 
+# Band Member Names
+
+
+def test_name_musician():
+  actual = Musician('John')
+  assert 'John' == actual.name
+
+def test_name_bass():
+  actual = Bassist('Paul')
+  assert 'Paul' == actual.name
+
+def test_name_guitar():
+  actual = Guitarist('George')
+  assert 'George' == actual.name
+
+def test_name_drummer():
+  actual = Drummer('Ringo')
+  assert 'Ringo' == actual.name
+
+
+# Get Instrument Tests
+
+
+def test_musician_instrument():
+  actual = Musician('Name')
+  assert 'Nothing' == actual.get_instrument()
+
+def test_get_guitar():
+  actual = Guitarist('Slash')
+  assert 'Guitar' == actual.get_instrument()
+
+def test_get_bass():
+  actual = Bassist('Who Plays Bass Again?')
+  assert 'Bass' == actual.get_instrument()
+
+def test_get_drums():
+  actual = Drummer('Insert Name')
+  assert 'Drums' == actual.get_instrument()
+
+
+# Test Solos
+
+
+def test_guitar_solo():
+  actual = Guitarist('An Actual Ukulele')
+  assert 'Guitar Sounds' == actual.play_solo()
+
+def test_bass_solo():
+  actual = Bassist('A Really Big Ukulele')
+  assert 'Bass Sounds' == actual.play_solo()
+
+def test_drum_solo():
+  actual = Drummer('Silence')
+  assert 'LOUD NOISES' == actual.play_solo()
+
+def test_all_band_solos():
+  actual = Band('Test Band', [Guitarist('Wham'), Drummer('Bam'), Bassist('Shang-a-lang')])
+  assert 'Guitar Sounds LOUD NOISES Bass Sounds ' == actual.play_solos()
+
+band_one = """
+Catfish and the Bottlemen
+Van McCann,Guitar
+Jon Barr,Drums
+Benji Blakeway,Bass
+Bob Hall,Guitar
+"""
+band_two = """
+Louis the Child
+Robby,Bass
+Freddy,Bass
+"""
+
+#  Output Tests
+
+def test_str_output():
+  test_band = Band.create_from_data(band_one)
+  expected = """The Band's Name is: Catfish and the Bottlemen
+The Members are:
+Van McCann on Guitar
+Jon Barr on Drums
+Benji Blakeway on Bass
+Bob Hall on Guitar
+"""
+  assert expected == test_band.__str__()
+
+def test_diff_str_output():
+  test_band = Band.create_from_data(band_two)
+  expected = """The Band's Name is: Louis the Child
+The Members are:
+Robby on Bass
+Freddy on Bass
+"""
+  assert expected == test_band.__str__()
+
+def test_all_bands_list():
+  Band.create_from_data(band_one)
+  Band.create_from_data(band_two)
+  expected = """The Band's Name is: Catfish and the Bottlemen
+The Members are:
+Van McCann on Guitar
+Jon Barr on Drums
+Benji Blakeway on Bass
+Bob Hall on Guitar
+The Band's Name is: Louis the Child
+The Members are:
+Robby on Bass
+Freddy on Bass
+"""
+  assert expected == Band.to_list()
